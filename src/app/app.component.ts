2d48aabd8300d51edc26c561005ec73a84f51e2c
Jessica Scheeler <jessica.n.scheeler@pwc.com>
2018-06-05 14:43:28 -0400

    Added ability to assign x or o to square

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,66 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  teams = [];
+  currentTeams = [];
+  activeTeam: string;
+  squares = [
+    {
+      celebrity: 'Carlos',
+      image: '/assets/carlos.jpg',
+      won: null
+    },
+    {
+      celebrity: 'Ivan',
+      image: '/assets/ivan.jpg',
+      won: null
+    },
+    {
+      celebrity: 'Ben',
+      image: '/assets/ben.jpg',
+      won: null
+    },
+    {
+      celebrity: 'Warren',
+      image: '/assets/warren.jpg',
+      won: null
+    },
+    {
+      celebrity: 'Travis / Kristy',
+      image: '/assets/kristy-travis.jpg',
+      won: null
+    },
+    {
+      celebrity: 'Christophe',
+      image: '/assets/christophe.png',
+      won: null
+    },
+    {
+      celebrity: 'Dave',
+      image: '/assets/david.jpg',
+      won: null
+    },
+    {
+      celebrity: 'Bill',
+      image: '/assets/bill.jpg',
+      won: null
+    },
+    {
+      celebrity: 'Pat',
+      image: '/assets/pat.jpg',
+      won: null
+    }
+  ];
+
+  constructor() {}
+
+  setSquare(i) {
+    console.log(i);
+    console.log(this.squares);
+    this.squares[i].won = this.activeTeam;
+  }
+
+  setActiveTeam(team) {
+    this.activeTeam = team;
+  }
 }
